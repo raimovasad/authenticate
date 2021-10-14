a96afe2dfc1e859c2eb0ba6e2be93e59ebcd3283
raimovasadbek <raimovasadbek18@gmail.com>
2021-10-14 19:11:45 +0500

    add

--- controllers/auth.js
+++ controllers/auth.js
@@ -76,13 +76,33 @@ exports.forgotpassword = async(req,res,next)=>{
 }
 
 
-exports.resetpassword = (req,res,next)=>{
-    const resetToken = crypto.createHash()
-    res.send('resetpassword route')
+exports.resetpassword = async(req,res,next)=>{
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resetToken).digest("hex")
+    try{
+        const user  =await User.findOne({
+            resetPasswordToken: resetPasswordToken,
+            resetPasswordExpire:{$gt: Date.now()}
+        })
+        console.log(resetPasswordToken ,'546a60c95f75550481ee4a6f499fcc4f492e0bbc1141cce7049a4056dc1455ee');
+        if(!user){
+            return next(new ErrorResponse("Invalid Reset Token",400))
+        }
+        user.password = req.body.password
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpire = undefined
+        await user.save()
+        res.status(201).json({
+            success: true,
+            data: 'Password Reset Success'
+        })
+    }
+    catch(e){
+        next(e)
+    }
 }
 
 const sendToken = async(user,statusCode,res)=>{
     const token = await user.getSignedToken()
     console.log(token);
     res.status(statusCode).json({success: true,token})
-}
+} 
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,25 +1,48 @@
 const nodemailer = require('nodemailer')
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendEmail = (options)=>{
-    const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE,
-        auth:{
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    })
-    const mailOptions = {
-        from:process.env.EMAIL_FROM,
-        to:options.to,    
-        subject: options.subject,
-        html:options.text
-    }
-    transporter.sendMail(mailOptions,function(err,info){
-        if(err){
-            console.log(err);
-        }else{
-            console.log(info);
-        }
-    })
+    const msg = {
+        to: options.to, // Change to your recipient
+        from: process.env.EMAIL_FROM, // Change to your verified sender
+        subject: 'Reset your password Asadbek',
+        text: "Change IT academy",
+        html: options.text,
+      }
+      sgMail
+        .send(msg)
+        .then(() => {
+          console.log('Email sent')
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+
 }
+
+
+  
+// const sendEmail = (options)=>{
+//     const transporter = nodemailer.createTransport({
+//         service: process.env.EMAIL_SERVICE,
+//         auth:{
+//             user: process.env.EMAIL_USERNAME,
+//             pass: process.env.EMAIL_PASSWORD
+//         }
+//     })
+//     const mailOptions = {
+//         from:process.env.EMAIL_FROM,
+//         to:options.to,    
+//         subject: options.subject,
+//         html:options.text
+//     }
+//     transporter.sendMail(mailOptions,function(err,info){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log(info);
+//         }
+//     })
+// }
 
 module.exports = sendEmail
