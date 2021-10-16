83dea2e21e96207cecc9cb65d35845542d61518f
raimovasad <88774509+raimovasad@users.noreply.github.com>
2021-10-16 08:17:58 +0500

    as

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT ||3000
 
-const server = app.listen(PORT,()=>  console.log(`Server is running on port ${PORT}`))
+const server = app.listen(PORT,()=>  console.log(`Server is running on port ${PORT} 😊 👌`))
 
 process.on('unhandledRejection',(err,promise)=>{
     console.log(`Logged error ${err}`);
